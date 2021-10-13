962c59b68dfa26cd04cc38cef7408c19f6d748a9
Malissa Bishop <malissambishop7@gmail.com>
2021-10-12 21:37:13 -0700

    commit tree 870866b535b7361a10e45454426dc708fed5c094

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 1;
+const num2 = 62;
 
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() > 0);
+
+const num3 = 32
+const num4 = 8
+let mod = num3 / num4;
+
+let max = Math.max(5, 10, 15, 20);
 // Write your code here
